f1ce42227521bd8cd0a370a3cca8bc33448ac338
Utsav Kashichhwa <utsavk0112@gmail.com>
2024-01-27 15:21:03 +1100

    Add basic operation functions

---
+++ script.js
@@ -0,0 +1,19 @@
+let total = 0;
+
+// OPERATIONS
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
