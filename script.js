e7050033afd18a83f00590f46f1a6e78684eda32
UtsavK-0112 <99528866+UtsavK-0112@users.noreply.github.com>
2024-02-10 19:42:12 +1100

    Add simple operators

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const displayText = document.querySelector(".--display-text");
+
+const calculatorButtons = document.querySelectorAll(".--calculator-button");
+const numberButtons = document.querySelectorAll(".--calculator-button.number");
+const clearButton = document.querySelector(".--calculator-button#clear");
+const negativeButton = document.querySelector(".--calculator-button#negative");
+const percentageButton = document.querySelector(
+    ".--calculator-button#percentage"
+);
+
 let total = 0;
 
 // OPERATIONS
@@ -17,3 +27,49 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+// SIMPLE OPERATIONS
+
+function percentage(a) {
+    return a * 0.01;
+}
+
+function negative(a) {
+    return -a;
+}
+
+function clear() {
+    return 0;
+}
+
+// FUNCTIONS
+function getTotal() {
+    return total;
+}
+
+function setTotal(newTotal) {
+    total = newTotal;
+    displayText.innerText = total;
+}
+
+// EVENT LISTENERS
+console.log(numberButtons);
+for (numberButton of numberButtons) {
+    numberButton.addEventListener("click", (event) => {
+        const number = parseInt(event.target.innerText);
+        total;
+        setTotal(getTotal() * 10 + number);
+    });
+}
+
+clearButton.addEventListener("click", (event) => {
+    setTotal(clear());
+});
+
+negativeButton.addEventListener("click", (event) => {
+    setTotal(negative(getTotal()));
+});
+
+percentageButton.addEventListener("click", (event) => {
+    setTotal(percentage(getTotal()));
+});
